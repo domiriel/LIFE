c179139cebff8f0b38b63e865f607169b6803388
domiriel <daniel.goncalves@inesc-id.pt>
2016-07-26 17:37:19 +0100

    Created setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'life',
+  packages = ['life'], # this must be the same as the name above
+  version = '1.1',
+  description = 'Library to handle the LIFE lifelogging format for recording personal location and travel information.',
+  author = 'Daniel Gonçalves',
+  author_email = 'daniel.goncalves@inesc-id.pt',
+  url = 'https://github.com/domiriel/LIFE', 
+  download_url = 'https://github.com/peterldowns/mypackage/tarball/0.1', # I'll explain this in a second
+  keywords = ['lifelogging', 'location', 'travel', 'stays', 'movement', 'gps', 'tracking', 'quantified self'],
+  classifiers = [],
+)
